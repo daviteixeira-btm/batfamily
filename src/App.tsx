b5cf27985bd8898754a3857d6778d4ac65d37775
Davi Teixeira <daviteixeira.btm@gmail.com>
2022-06-28 17:48:07 -0300

    iniciando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Seja bem-vindo a BatFamily!</p>
+        <p> Coming soon...</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
